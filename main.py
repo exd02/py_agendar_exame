2ec9025664ef223b14485bc212b0091745bb0965
exd02 <eliamxd1337@gmail.com>
2025-01-18 13:12:38 -0300

    adicionada logica de coloracao para separar os grupos de horarios dos exames

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+#TODO, calcular a matriz de horários vagos de seg/sex e depois criar a matriz com os dias necessários
 def find_common_free_slots_matrix(schedules, days_exam):
     """
     Retorna os horários vagos comuns para os dias especificados.
@@ -41,12 +45,12 @@ schedules = {
     ],
 }
 
-students_exam = {
-    "Aluno1": [1, 2, 3, 5],
-    "Aluno2": [2, 6],
-    "Aluno3": [2],
-    "Aluno4": [1, 7, 17, 20],
-}
+students_exam = [
+    [1, 2, 3, 5],
+    [2, 6],
+    [2],
+    [1, 2, 4, 6],
+]
 
 days_exam = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
 result = find_common_free_slots_matrix(schedules, days_exam)
@@ -57,10 +61,56 @@ for i, free_slots in enumerate(result):
 
 recovery_disciplines = set() # set nao aceita valores duplicados
 
-for disciplines in students_exam.values():
+for disciplines in students_exam:
     recovery_disciplines.update(disciplines)
 
 recovery_disciplines = sorted(recovery_disciplines)
 
-print(f"Disciplinas de recuperação: {', '.join(map(str, recovery_disciplines))}")
+print("Disciplinas na recuperação: ", recovery_disciplines)
 
+G = nx.Graph()
+
+# cada grupo é adicionado como um grafo completo
+for group in students_exam:
+    G.add_edges_from([(i, j) for i in group for j in group if i != j])
+
+degree_sequence = sorted(G.degree, key = lambda deg: deg[1], reverse=True)
+
+print("[+] Ordenando nós por grau:")
+for d in degree_sequence:
+    print("\t[-]", d[0], "--->", d[1])
+
+node_groups = {}
+used_groups = []
+
+for node, _ in degree_sequence:
+    # Horários dos vizinhos
+    neighbor_groups = [node_groups.get(neighbor) for neighbor in G.neighbors(node) if neighbor in node_groups]
+
+    # Pegar o primeiro horário disponível
+    available_group = next((group for group in range(len(result)) if group not in neighbor_groups), None)
+
+    if available_group is None:
+        print(f"O programa excedeu o número de horários permitidos no nó {node}.")
+        exit(1)
+
+    node_groups[node] = available_group
+    if available_group not in used_groups:
+        used_groups.append(available_group)
+
+print("[+] Grupos atribuídos por nó:")
+for n,g in node_groups.items():
+    print("\t[-]",n,"--->",g)
+
+# Desenhando o grafo
+pos = nx.spring_layout(G)
+nx.draw(
+    G,
+    pos,
+    with_labels=True,
+    node_color=[node_groups[node] for node in G.nodes()],
+    node_size=1000,
+    font_weight="bold",
+    cmap=plt.cm.Paired,
+)
+plt.show()
